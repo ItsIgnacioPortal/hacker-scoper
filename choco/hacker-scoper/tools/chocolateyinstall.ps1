6fbd84ada9757e3bd335b7375c4d09795ece877a
PinkDev1 <5990@protonmail.com>
2022-09-13 21:30:19 -0300

    updated chocolatey package to v3.0.0

--- choco/hacker-scoper/tools/chocolateyinstall.ps1
+++ choco/hacker-scoper/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = "$toolsDir"
-  file          = "$toolsDir\hacker-scoper_2.1.0_windows_386.zip"
-  file64        = "$toolsDir\hacker-scoper_2.1.0_windows_amd64.zip"
+  file          = "$toolsDir\hacker-scoper_3.0.0_windows_386.zip"
+  file64        = "$toolsDir\hacker-scoper_3.0.0_windows_amd64.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
