c327f148f4835cc7d8398a86e38801732066f739
PinkDev1 <5990@protonmail.com>
2022-10-04 23:54:53 -0300

    AutoReleaser: Test #3

--- choco/chocolateyinstall_template.ps1
+++ choco/chocolateyinstall_template.ps1
@@ -4,8 +4,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = "$toolsDir"
-  file          = "$toolsDir\hacker-scoper_VERSION-HERE_windows_386.zip"
-  file64        = "$toolsDir\hacker-scoper_VERSION-HERE_windows_amd64.zip"
+  file          = "$toolsDir\hacker-scoper_VERSIONHERE_windows_386.zip"
+  file64        = "$toolsDir\hacker-scoper_VERSIONHERE_windows_amd64.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
